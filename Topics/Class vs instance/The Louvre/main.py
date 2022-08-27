b1bc35df9406ef0ed49300c6fbc6b2d4ac1d9c03
David Itzhak <daviditzhak7@gmail.com>
2022-08-27 20:19:38 +0300

    27.08.2022 - 1

--- Topics/Class vs instance/The Louvre/main.py
+++ Topics/Class vs instance/The Louvre/main.py
@@ -1,1 +1,15 @@
 class Painting:
+    museum = 'Louvre'
+
+    def __init__(self, title_: str, artist_: str, year_: int):
+        self.title_: str = title_
+        self.artist_: str = artist_
+        self.year_: int = year_
+        print(f'"{self.title_}" by {self.artist_} ({self.year_}) hangs in the {Painting.museum}.')
+
+
+title: str = input()
+artist: str = input()
+year: int = int(input())
+
+some_painting = Painting(title, artist, year)
